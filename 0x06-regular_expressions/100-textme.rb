bd846ea9ce1f7d2ca7299b4cea4edad6daa5b558
Joseun <ologanj@gmail.com>
2022-01-19 00:38:01 +0000

    Regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([A-Za-z]+|\+?\d{11}?)\]\s\[to:([A-Za-z]+|\+?\d{11}?)\]\s\[flags:([-?\d:\d]+)\]/).join
+puts ARGV[0].scan(/\[from:([A-Za-z]+|\+?\d{11}?)\]\s\[to:([A-Za-z]+|\+?\d{11}?)\]\s\[flags:([-?\d:\d]+)\]/).join(',')
