1d26dce381524f52aa3ab410adaa2e019aebb9ff
Joseun <ologanj@gmail.com>
2022-03-11 16:33:29 +0000

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file { '/tmp/school':
+# Using Puppet, create a file in /tmp
+file { 'school':
   ensure  => 'file',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet",
+  content => 'I love Puppet',
   mode    => '0744',
+  path    => '/tmp'
 }
