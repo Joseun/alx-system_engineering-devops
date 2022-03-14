8e7b3161ba8cc1769de3aa60a22e68b122c6c690
Joseun <ologanj@gmail.com>
2022-03-14 01:49:00 +0000

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# edit configuration file for ssh connections
+
+$str = "Host 35.231.246.241
+	Hostname 35.231.246.241
+	IdentityFile	~/.ssh/school
+	PasswordAuthentication no"
+
+file { 'config':
+  ensure  => 'present',
+  path    => '/root/.ssh/config',
+  content => $str,
+}
