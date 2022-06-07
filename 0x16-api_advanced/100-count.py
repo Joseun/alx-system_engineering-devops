2879c424a4b817ae1cf7c8984292e4e3b161e3ac
Joseun <ologanj@gmail.com>
2022-06-07 09:17:18 +0000

    Working on left overwork, ALX is fun

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -43,7 +43,8 @@ def count_words(subreddit, word_list, instances={}, count=0, after=None):
         if after is None:
             if len(instances) == 0:
                 return (None)
-            instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+            instances = sorted(instances.items(),
+                               key=lambda kv: (-kv[1], kv[0]))
             [print("{}: {}".format(k, v)) for k, v in instances]
         else:
             return (count_words(subreddit, word_list, instances, count, after))
